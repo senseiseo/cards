a6bf4f587b71fc553ae383877bb1f833fc8b76fe
ildar <1919mur@gmail.com>
2021-06-23 08:28:36 +0300

    '1'

--- app/controllers/cardwords_controller.rb
+++ app/controllers/cardwords_controller.rb
@@ -55,27 +55,15 @@ class CardwordsController < ApplicationController
     @cardword.increment! :bad_response
       if  @cardword.bad_response == 10 
         @cardword.hard_word = true
-       
-
       end 
     @cardword.save
     start_viktorine
   end
     
-<<<<<<< Updated upstream
-  def start_viktorine #  методы обработки старта викторинры, нарушение всех принципов программировния )
-=======
-  def cardwords(number) 
-    @cardwords = Cardword.where(group: number ).last
-      if @cardwords.present?  and (@cardwords.time_to_next_appearance == 0 )
-        render :start
-      else
-         send('start_viktorine' + "#{number}")
-      end
-  end
 
-  def start_viktorine 
->>>>>>> Stashed changes
+ 
+
+  def start_viktorine #  методы обработки старта викторинры, нарушение всех принципов программировния )
      @cardwords = Cardword.where(group: 0 ).last
     if @cardwords.present?  and @cardwords.time_to_next_appearance == 0  and @cardwords.hard_word == false
       render :start
